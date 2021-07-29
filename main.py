2746ea7d00f1a94961c0f924bf0fb52dddb7ac34
Geno Ferollo <sakhib.orzklv@icloud.com>
2021-07-29 18:35:05 +0500

    Update main.py

--- main.py
+++ main.py
@@ -159,176 +159,47 @@ text_19 = "19. Абдулла Арипов, Ўзбекистон Республ�
 question_19 = mcq_question(table_frame, label_19_row, options_19_26,
                            text=text_19, num_options=4)
 
+# Question 20
+label_20_row = 2
+text_20 = "20. Танзила Нарбаева, Ўзбекистон Республикаси Олий Мажлиси Сенати раиси"
+question_20 = mcq_question(table_frame, label_20_row, options_19_26,
+                           text=text_20, num_options=4)
 
-# # Question 20
-# label_20_row = 4
-# label_20 = tk.Label(table_frame, text="20. Танзила Нарбаева, Ўзбекистон Республикаси Олий Мажлиси Сенати раиси.")
-# label_20.grid(row=label_20_row, column=0, padx=10, pady=10, columnspan=4)
-# question_20 = add_button(table_frame, label_20_row, options_19_26)
+# Question 21
+label_21_row = 2
+text_21 = "21. Нурдинжон Исмоилов, Ўзбекистон Республикаси Олий Мажлиси Қонунчилик палатасининг спикери"
+question_21 = mcq_question(table_frame, label_21_row, options_19_26,
+                           text=text_21, num_options=4)
 
-# # Question 21
-# label_21_row = 6
-# label_21 = tk.Label(table_frame, text="21. Нурдинжон Исмоилов, Ўзбекистон Республикаси Олий Мажлиси Қонунчилик палатасининг спикери")
-# label_21.grid(row=label_21_row, column=0, padx=10, pady=10, columnspan=4)
-# options_21 = ["Тўлиқ ишонаман", "Ишонаман", "Ишонмайман", "Тўлиқ ишонмайман"]
-# question_21 = add_button(table_frame, label_21_row, options_21)
+# Question 22
+label_22_row = 2
+text_22 = "22. Актам Хаитов, Ўзбекистон Либерал-демократик партияси етакчиси"
+question_22 = mcq_question(table_frame, label_22_row, options_19_26,
+                           text=text_22, num_options=4)
 
+# Question 23
+label_23_row = 2
+text_23 = "23. Алишер Қодиров, Ўзбекистон «Миллий тикланиш» демократик партияси етакчиси"
+question_23 = mcq_question(table_frame, label_23_row, options_19_26,
+                           text=text_23, num_options=4)
 
+# Question 24
+label_24_row = 2
+text_24 = "24. Баҳром Абдухалимов, Ўзбекистон «Адолат»  демократик партияси етакчиси "
+question_24 = mcq_question(table_frame, label_24_row, options_19_26,
+                           text=text_24, num_options=4)
 
+# Question 25
+label_25_row = 2
+text_25 = "25. Улуғбек Иноятов, Ўзбекистон Халқ демократик партияси етакчиси"
+question_25 = mcq_question(table_frame, label_25_row, options_19_26,
+                           text=text_25, num_options=4)
 
-
-
-
-# button_20_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_20)
-# button_20_1.grid(row=4, column=1, padx=10, pady=10)
-# button_20_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_20)
-# button_20_2.grid(row=4, column=2, padx=10, pady=10)
-# button_20_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_20)
-# button_20_3.grid(row=4, column=3, padx=10, pady=10)
-# button_20_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_20)
-# button_20_4.grid(row=4, column=4, padx=10, pady=10)
-
-# # Question 21
-# question_21 = tk.IntVar(table_frame)
-# label_21 = tk.Label(table_frame, text="21. Нурдинжон Исмоилов, Ўзбекистон Республикаси Олий Мажлиси Қонунчилик палатасининг спикери")
-# label_21.grid(row=4, column=0, padx=10, pady=10)
-# button_21_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_21)
-# button_21_1.grid(row=5, column=1, padx=10, pady=10)
-# button_21_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_21)
-# button_21_2.grid(row=5, column=2, padx=10, pady=10)
-# button_21_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_21)
-# button_21_3.grid(row=5, column=3, padx=10, pady=10)
-# button_21_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_21)
-# button_21_4.grid(row=5, column=4, padx=10, pady=10)
-
-# # Question 22
-# question_22 = tk.IntVar(table_frame)
-# label_22 = tk.Label(table_frame, text="22. Актам Хаитов, Ўзбекистон Либерал-демократик партияси етакчиси")
-# label_22.grid(row=6, column=0, padx=10, pady=10)
-# button_22_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_22)
-# button_22_1.grid(row=7, column=1, padx=10, pady=10)
-# button_22_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_22)
-# button_22_2.grid(row=7, column=2, padx=10, pady=10)
-# button_22_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_22)
-# button_22_3.grid(row=7, column=3, padx=10, pady=10)
-# button_22_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_22)
-# button_22_4.grid(row=7, column=4, padx=10, pady=10)
-
-# # Question 23
-# question_23 = tk.IntVar(table_frame)
-# label_23 = tk.Label(table_frame, text="23. Алишер Қодиров, Ўзбекистон «Миллий тикланиш» демократик партияси етакчиси ")
-# label_23.grid(row=8, column=0, padx=10, pady=10)
-# button_23_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_23)
-# button_23_1.grid(row=9, column=1, padx=10, pady=10)
-# button_23_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_23)
-# button_23_2.grid(row=9, column=2, padx=10, pady=10)
-# button_23_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_23)
-# button_23_3.grid(row=9, column=3, padx=10, pady=10)
-# button_23_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_23)
-# button_23_4.grid(row=9, column=4, padx=10, pady=10)
-
-# # Question 24
-# question_24 = tk.IntVar(table_frame)
-# label_24 = tk.Label(table_frame, text="24. Баҳром Абдухалимов, Ўзбекистон «Адолат»  демократик партияси етакчиси ")
-# label_24.grid(row=10, column=0, padx=10, pady=10)
-# button_24_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_24)
-# button_24_1.grid(row=11, column=1, padx=10, pady=10)
-# button_24_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_24)
-# button_24_2.grid(row=11, column=2, padx=10, pady=10)
-# button_24_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_24)
-# button_24_3.grid(row=11, column=3, padx=10, pady=10)
-# button_24_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_24)
-# button_24_4.grid(row=11, column=4, padx=10, pady=10)
-
-# # Question 25
-# question_25 = tk.IntVar(table_frame)
-# label_25 = tk.Label(table_frame, text="25. Улуғбек Иноятов, Ўзбекистон Халқ демократик партияси етакчиси ")
-# label_25.grid(row=12, column=0, padx=10, pady=10)
-# button_25_1 = tk.Radiobutton(table_frame, text="1", value=1, variable=question_25)
-# button_25_1.grid(row=13, column=1, padx=10, pady=10)
-# button_25_2 = tk.Radiobutton(table_frame, text="2", value=2, variable=question_25)
-# button_25_2.grid(row=13, column=2, padx=10, pady=10)
-# button_25_3 = tk.Radiobutton(table_frame, text="3", value=3, variable=question_25)
-# button_25_3.grid(row=13, column=3, padx=10, pady=10)
-# button_25_4 = tk.Radiobutton(table_frame, text="4", value=4, variable=question_25)
-# button_25_4.grid(row=13, column=4, padx=10, pady=10)
-
-# # Question 26
-# question_26 = tk.IntVar(table_frame)
-# label_26 = tk.Label(prezident_frame, text="26. Нарзулло Обломуродов, Ўзбекистон Экология партияси етакчиси ")
-# label_26.grid(row=14, column=0, padx=10, pady=10)
-# button_26_1 = tk.Radiobutton(prezident_frame, text="1", value=1, variable=question_26)
-# button_26_1.grid(row=15, column=1, padx=10, pady=10)
-# button_26_2 = tk.Radiobutton(prezident_frame, text="2", value=2, variable=question_26)
-# button_26_2.grid(row=15, column=2, padx=10, pady=10)
-# button_26_3 = tk.Radiobutton(prezident_frame, text="3", value=3, variable=question_26)
-# button_26_3.grid(row=15, column=3, padx=10, pady=10)
-# button_26_4 = tk.Radiobutton(prezident_frame, text="4", value=4, variable=question_26)
-# button_26_4.grid(row=15, column=4, padx=10, pady=10)
-
-# # Question 27
-# info_27 = tk.LabelFrame(root, borderwidth=0, highlightthickness=0, padx=10, pady=10)
-# info_27.pack()
-# question_27 = tk.IntVar(info_27)
-# label_27 = tk.Label(info_27, text="27.	Президент Шавкат Мирзиёев ўз лавозимида фикрингиз бўйича қандай фаолият кўрсатаётганлигини 7 баллик шкалада баҳоланг. (1 - энг паст баҳо, 7 - энг юқори баҳо )")
-# label_27.grid(row=4, column=0, padx=10, pady=10)
-# button_27_1 = tk.Radiobutton(info_27, text="1", value=1, variable=question_27)
-# button_27_1.grid(row=4, column=1, padx=10, pady=10)
-# button_27_2 = tk.Radiobutton(info_27, text="2", value=2, variable=question_27)
-# button_27_2.grid(row=4, column=2, padx=10, pady=10)
-# button_27_3 = tk.Radiobutton(info_27, text="3", value=3, variable=question_27)
-# button_27_3.grid(row=4, column=3, padx=10, pady=10)
-# button_27_4 = tk.Radiobutton(info_27, text="4", value=4, variable=question_27)
-# button_27_4.grid(row=4, column=4, padx=10, pady=10)
-# button_27_5 = tk.Radiobutton(info_27, text="5", value=5, variable=question_27)
-# button_27_5.grid(row=4, column=4, padx=10, pady=10)
-# button_27_6 = tk.Radiobutton(info_27, text="6", value=6, variable=question_27)
-# button_27_6.grid(row=4, column=4, padx=10, pady=10)
-# button_27_7 = tk.Radiobutton(info_27, text="7", value=7, variable=question_27)
-# button_27_7.grid(row=4, column=4, padx=10, pady=10)
-
-# # Question 28
-# info_28 = tk.LabelFrame(root, borderwidth=0, highlightthickness=0, padx=10, pady=10)
-# info_28.pack()
-# question_28 = tk.IntVar(info_28)
-# question_28.set("Президент")
-# label_28 = tk.Label(info_28, text="28. Ўтган икки ой мобайнида Шавкат Мирзиёевга Ўзбекистон Президенти сифатида муносабатингиз ўзгардими? Ва, агар ўзгарган бўлса, у қайси томонга - яхшиланди ёки ёмонлашди? (битта жавоб)")
-# label_28.grid(row=4, column=0, padx=10, pady=10)
-# button_28_1 = tk.Radiobutton(info_28, text="1. Анча яхшиланди", value=1, variable=question_28)
-# button_28_1.grid(row=4, column=1, padx=10, pady=10)
-# button_28_2 = tk.Radiobutton(info_28, text="2. Озроқ яхшиланди", value=2, variable=question_28)
-# button_28_2.grid(row=4, column=2, padx=10, pady=10)
-# button_28_3 = tk.Radiobutton(info_28, text="3. Озроқ ёмонлашди", value=3, variable=question_28)
-# button_28_3.grid(row=4, column=3, padx=10, pady=10)
-# button_28_4 = tk.Radiobutton(info_28, text="4. Анча ёмонлашди", value=4, variable=question_28)
-# button_28_4.grid(row=4, column=4, padx=10, pady=10)
-# button_28_5 = tk.Radiobutton(info_28, text="5. Ҳеч нарса ўзгармади", value=5, variable=question_28)
-# button_28_5.grid(row=4, column=4, padx=10, pady=10)
-# button_28_6 = tk.Radiobutton(info_28, text="6. (БУНИ ЎҚИМАНГ) Жавоб беришга қийналаман", value=6, variable=question_28)
-# button_28_6.grid(row=4, column=4, padx=10, pady=10)
-
-# # Question 29
-# info_29 = tk.LabelFrame(root, borderwidth=0, highlightthickness=0, padx=10, pady=10)
-# info_29.pack()
-# question_29 = tk.IntVar(info_28)
-# question_29.set("Президент")
-# label_29 = tk.Label(info_28, text="28. Ўтган икки ой мобайнида Шавкат Мирзиёевга Ўзбекистон Президенти сифатида муносабатингиз ўзгардими? Ва, агар ўзгарган бўлса, у қайси томонга - яхшиланди ёки ёмонлашди? (битта жавоб)")
-# label_29.grid(row=4, column=0, padx=10, pady=10)
-# button_29_1 = tk.Radiobutton(info_28, text="1. Анча яхшиланди", value=1, variable=question_28)
-# button_29_1.grid(row=4, column=1, padx=10, pady=10)
-# button_29_2 = tk.Radiobutton(info_28, text="2. Озроқ яхшиланди", value=2, variable=question_28)
-# button_29_2.grid(row=4, column=2, padx=10, pady=10)
-# button_29_3 = tk.Radiobutton(info_28, text="3. Озроқ ёмонлашди", value=3, variable=question_28)
-# button_29_3.grid(row=4, column=3, padx=10, pady=10)
-# button_29_4 = tk.Radiobutton(info_28, text="4. Анча ёмонлашди", value=4, variable=question_28)
-# button_29_4.grid(row=4, column=4, padx=10, pady=10)
-# button_29_5 = tk.Radiobutton(info_28, text="5. Ҳеч нарса ўзгармади", value=5, variable=question_28)
-# button_29_5.grid(row=4, column=4, padx=10, pady=10)
-# button_29_6 = tk.Radiobutton(info_28, text="6. (БУНИ ЎҚИМАНГ) Жавоб беришга қийналаман", value=6, variable=question_28)
-# button_29_6.grid(row=4, column=4, padx=10, pady=10)
-
-
-
+# Question 26
+label_26_row = 2
+text_26 = "26. Нарзулло Обломуродов, Ўзбекистон Экология партияси етакчиси"
+question_26 = mcq_question(table_frame, label_26_row, options_19_26,
+                           text=text_26, num_options=4)
 
 # major frame
 major_frame = tk.LabelFrame(root, borderwidth=0, highlightthickness=0, padx=10, pady=10)
